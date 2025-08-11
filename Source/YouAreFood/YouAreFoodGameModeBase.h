b347a809a05ff3667f6f61df11cb2ca664c9411f
Stephen1902 <s_nichols_other@yahoo.co.uk>
2025-08-11 15:26:19 +0100

    Initial Commit

---
+++ Source/YouAreFood/YouAreFoodGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YouAreFoodGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YOUAREFOOD_API AYouAreFoodGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
